c6aa1017d8b6552f4f389ae3e0774476ebdcd9bc
Logan Marin <logapmarin@gmail.com>
2020-05-09 03:07:34 -0500

    About section title and styling. Also added the about me content.

--- src/app/landing-page/about/about.component.ts
+++ src/app/landing-page/about/about.component.ts
@@ -9,11 +9,16 @@ import { ViewportRuler } from '@angular/cdk/scrolling';
 export class AboutComponent implements OnInit {
 
     public viewportHeight: number;
+    public heroSectionBorderCount: number;
 
     constructor(private viewportRuler: ViewportRuler) { }
 
     ngOnInit(): void {
         this.viewportHeight = this.viewportRuler.getViewportSize().height;
+
+        // TODO: Recalc with window resize event;
+        // Based on viewport width and 10% border width, calculate how many icons we can fit sized at .4rem and spaced with .5rem (i.e .9);
+        this.heroSectionBorderCount = Math.ceil((this.viewportRuler.getViewportSize().width * .1) / (16 * .9));
     }
 
 }
